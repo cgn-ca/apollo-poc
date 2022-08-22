cd4fae590c40a6e17639dda14b8390faf138d2f1
Colin Nieuwenhuizen <colin.nieuwenhuizen@allergan.com>
2022-08-22 08:40:54 -0600

    Adding middleware logging context

--- logger.ts
+++ logger.ts
@@ -31,3 +31,10 @@ export const withUserContext = <T extends string, F extends string>(resolver: Gr
     store.set('logger', child)
     return context.run(store, () => resolver(args[0], args[1], args[2], args[3]))
 }
+
+export const contextMiddleware = (req, res, next) => {
+    const child = exportedLogger.child({"user": "some-user-pulled-off-context"})
+    const store = new Map()
+    store.set('logger', child)
+    return context.run(store, next)
+};
